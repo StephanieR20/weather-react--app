4a6e824b5aec5c6718e0bf3e750124ecaede3330
StephanieR20 <67565105+StephanieR20@users.noreply.github.com>
2021-05-21 12:13:56 -0400

    move code to main folders, change the name for of the folders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"
+import Weather from "./Weather";
 
 
 
@@ -9,7 +10,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <App/>
+      <Weather/>
      </div>
   );
 }
